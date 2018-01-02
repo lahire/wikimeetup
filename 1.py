dc1ed7cd187638a2008b180e673ae1c9cf4833da
Nicolas Giorgetti <ngiorgetti@exo.com.ar>
2018-01-02 12:14:41 -0300

    mostrar resultados

--- 1.py
+++ 1.py
@@ -1,24 +1,28 @@
-
 import pywikibot
 
 
-site = pywikibot.Site("es", "wikipedia") #Busco elemento en es.wiki
+SITE = pywikibot.Site("es", "wikipedia") #Busco elemento en es.wiki
 try:
-    key = input('Ingrese lo que se quiera buscar en es.wiki')
+    KEY = input('Ingrese lo que se quiera buscar en es.wiki:\n')
 except:
     print('Escriba algo coherente :-(')
 
-if key == '':
-    key = 'Douglas Adams'
+if KEY == '':
+    KEY = 'Douglas Adams'
 
-page = pywikibot.Page(site, key)
+PAGE = pywikibot.Page(SITE, KEY)
 # >>> print(page)
 # [[wikipedia:es:Douglas Adams]]
 #Esto no tira error si la página no existe (busca links rojos)
 
 try:
-    item = pywikibot.ItemPage.fromPage(page)
+    ITEM = pywikibot.ItemPage.fromPage(PAGE)
     # >>> print(item)
     # [[wikidata:Q42]]
 except pywikibot.exceptions.NoPage:
     print("El ítem buscado no existe en Wikidata. ¿Por qué no lo creas?")
+
+print(ITEM)
+print(dir(ITEM))
+item_dict = ITEM.get()
+print(item_dict.keys())
